60fc6447c4baf796320d85a56382ec30fd566438
unknown <lucasmontero.wed.odin@gmail.com>
2022-08-14 13:11:42 -0300

    upload the rest

--- Web A/src/index.tsx
+++ Web A/src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './features/redux/store';
+import '../src/features/socketConnection/connection'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- Web B/src/index.tsx
+++ Web B/src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './features/redux/store';
-
+import '../src/features/socketConnection/connection'
 
 
 const root = ReactDOM.createRoot(
