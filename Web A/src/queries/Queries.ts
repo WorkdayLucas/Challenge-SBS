6a9e73a3bf32b21744eb949f1e98f208b1ad61e4
unknown <lucasmontero.wed.odin@gmail.com>
2022-08-14 13:00:34 -0300

    feat/ add queries and graphQLClient to web A

---
+++ Web A/src/queries/Queries.ts
@@ -0,0 +1,50 @@
+import { GraphQLClient, gql } from 'graphql-request'
+
+const endpoint = 'http://localhost:3001/api-products/'
+
+export const graphQLClient = new GraphQLClient(endpoint, {
+    headers: {
+        authorization: 'Bearer MY_TOKEN',
+    },
+})
+
+export const getProducts = gql`
+{
+  products {
+    _id
+    name
+    description
+    price
+    img
+  }
+}
+`
+
+export const createProduct = gql`
+    mutation CreateProduct($name: String!, $description: String!, $price: Int!, $img: String!) {
+        createProduct (input: {
+            name: $name,
+            description: $description
+            price: $price
+            img: $img
+            }) {
+            _id
+            name
+            description
+            price
+            img
+        }
+    }
+`
+
+export const deleteProduct = gql`
+    mutation DeleteProduct($_id: ID!) {
+        deleteProduct (_id: $_id) {
+            _id
+            name
+            description
+            price
+            img
+        }
+    }
+`
