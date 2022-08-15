01332a12619ab9973ba3026cf15791fa297e39fb
unknown <lucasmontero.wed.odin@gmail.com>
2022-08-14 21:17:32 -0300

    render navbar in A

--- Web A/src/App.tsx
+++ Web A/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home';
+import NavBar from './components/NavBar';
 import Products from './components/Products';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Home/>
     </div>
   );
